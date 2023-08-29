b6a1f96963e0cb3f8856f64c94014160ae7b4903
steve-237 <steve0peg@gmail.com>
2023-08-29 20:15:07 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIRACUM_Mapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
