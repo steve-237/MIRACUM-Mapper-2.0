164a560050f1143e85cf605d4f184e12e4a82f48
steve-237 <steve0peg@gmail.com>
2023-09-06 14:13:44 +0200

    Implemented the landing page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,24 @@ namespace MIRACUM_Mapper.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var elements = new List<ElementModel>
+            {
+                new ElementModel { Id = 1, Titre = "Élément 1" , Version = 1},
+                new ElementModel { Id = 2, Titre = "Élément 2", Version = 5 },
+                new ElementModel { Id = 3, Titre = "jean 1" , Version = 7},
+                new ElementModel { Id = 4, Titre = "paul 2", Version = 0 },
+                new ElementModel { Id = 5, Titre = "eli 1" , Version = 4},
+                new ElementModel { Id = 6, Titre = "vent 2" , Version = 3},
+                new ElementModel { Id = 7, Titre = "chat 1" , Version = 1},
+                new ElementModel { Id = 8, Titre = "paul 2" , Version = 1},
+                new ElementModel { Id = 3, Titre = "jean 1" , Version = 1},
+                new ElementModel { Id = 4, Titre = "paul 2" , Version = 1},
+                new ElementModel { Id = 5, Titre = "eli 1" , Version = 1},
+                new ElementModel { Id = 6, Titre = "vent 2" , Version = 1},
+                new ElementModel { Id = 7, Titre = "chat 1" , Version = 1},
+                new ElementModel { Id = 8, Titre = "paul 2", Version = 1},
+            };
+            return View(elements);
         }
 
         public IActionResult Privacy()
---
+++ Models/ElementModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MIRACUM_Mapper.Models
+{
+    public class ElementModel
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public float Version { get; set; }
+    }
+}
