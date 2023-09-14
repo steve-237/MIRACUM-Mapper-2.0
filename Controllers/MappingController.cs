b6101ca59f896e0cb5ede4b16bbbdec5d282eb20
steve-237 <steve0peg@gmail.com>
2023-09-14 19:06:23 +0200

    add link to see the detail of a project

---
+++ Controllers/MappingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIRACUM_Mapper.Models;
+
+namespace MIRACUM_Mapper.Controllers
+{
+    public class MappingController : Controller
+    {
+        private List<Project> elements = new List<Project>
+        {
+             new Project { Id = 1, Name = "Internal LabCode to LOINC Mapping", Description = "Decription 1", Version = 1.0f, Sources = {}, Targets = {} },
+             new Project { Id = 2, Name = "SNOMED to ICD Mapping", Description = "Decription 2", Version = 1.9f, Sources = {}, Targets = {}},
+             new Project { Id = 3, Name = "ICD-10 to LOINC Mapping", Description = "Decription 3", Version = 0.8f, Sources = {}, Targets = {}},
+             new Project { Id = 4, Name = "ICD-10 to SNOMED-CT Mapping", Description = "Decription 4", Version = 2.8f, Sources = {}, Targets = {}}
+        };
+        public IActionResult Index(int id)
+        {
+            var project = elements.FirstOrDefault(e => e.Id == id);
+            return View(project);
+        }
+
+        public List<Project> GetAllProjects()
+        {
+            return elements;
+        }
+    }
+}
--- Models/Project.cs
+++ Models/Project.cs
@@ -8,6 +8,7 @@ namespace MIRACUM_Mapper.Models
         [Required]
         public string Name { get; set; }
         public float Version { get; set; }
+        public string Description { get; set; }
         public List<Source>? Sources { get; set; }
         public List<Target>? Targets { get; set; }
     }
