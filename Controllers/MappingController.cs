6abc5b4abf33d27799cdbea2abf8ce2359b44106
steve-237 <steve0peg@gmail.com>
2023-09-15 23:43:40 +0200

    generate new field by clicking on + on create new project modal

--- Controllers/MappingController.cs
+++ Controllers/MappingController.cs
@@ -15,6 +15,10 @@ namespace MIRACUM_Mapper.Controllers
         public IActionResult Index(int id)
         {
             var project = elements.FirstOrDefault(e => e.Id == id);
+            if (project == null)
+            {
+                return View("Error");
+            }
             return View(project);
         }
 
