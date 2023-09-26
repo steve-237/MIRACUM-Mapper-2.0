ba0bc5c0a94f75123f0fd16439b4233fe430c62b
steve-237 <steve0peg@gmail.com>
2023-09-26 23:33:16 +0200

    add mapping table

--- db/db.cs
+++ db/db.cs
@@ -18,12 +18,23 @@ namespace MIRACUM_Mapper.db
             new Target { Id = 3, Name = "LONIC target", Description = "Description target 3", Url = "Url 3"},
         };
 
+        public static List<Source> sources1 = new List<Source>
+        {
+            new Source { Id = 1, Name = "ICD-11", Description = "Description source 1", Url = "Url 1"}
+        };
+
+        public static List<Target> targets1 = new List<Target>
+        {
+            new Target { Id = 1, Name = "UMLS target", Description = "Description target 1", Url = "Url 1"}
+        };
+
         public static List<Project> elements = new List<Project>
         {
              new Project { Id = 1, Name = "Internal LabCode to LOINC Mapping", Description ="description id1", Version = 1.0f, Sources = sources, Targets = targets, DisplayMappingEquivalence= true, DisplayStatus=false},
              new Project { Id = 2, Name = "SNOMED to ICD Mapping", Description ="description id2",Version = 1.9f, Sources = sources, Targets = targets, DisplayMappingEquivalence= true, DisplayStatus=true},
              new Project { Id = 3, Name = "ICD-10 to LOINC Mapping", Description ="description id3",Version = 0.8f, Sources = sources, Targets = targets, DisplayMappingEquivalence= false, DisplayStatus=false},
-             new Project { Id = 4, Name = "ICD-10 to SNOMED-CT Mapping",Description ="description id4", Version = 2.8f, Sources = sources, Targets = targets, DisplayMappingEquivalence= false, DisplayStatus=true}
+             new Project { Id = 4, Name = "ICD-10 to SNOMED-CT Mapping",Description ="description id4", Version = 2.8f, Sources = sources, Targets = targets, DisplayMappingEquivalence= false, DisplayStatus=true},
+             new Project { Id = 5, Name = "ICD-11 to UMLS Mapping",Description ="description id5", Version = 5.8f, Sources = sources1, Targets = targets1, DisplayMappingEquivalence= false, DisplayStatus=true}
         };
     }
 }
