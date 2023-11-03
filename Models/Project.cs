229a16d8556a547919322a67ff56ecf63dca484c
steve-237 <steve0peg@gmail.com>
2023-11-03 21:54:29 +0100

    add validator on create project form

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,10 +1,18 @@
-﻿namespace MIRACUM_Mapper.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MIRACUM_Mapper.Models
 {
     public class Project
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Version is required")]
         public float Version { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
         public bool DisplayMappingEquivalence { get; set; }
         public bool DisplayStatus { get; set; }
